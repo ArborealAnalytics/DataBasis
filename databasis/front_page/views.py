f08be4b6dfc8f614870e9aaf741736b4405aab55
Brylie Christopher Oxley <brylie@amble.fi>
2020-09-23 22:26:26 +0300

    Initial front page app

---
+++ databasis/front_page/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import loader
+
+
+def index(request):
+    context = {}
+    template = loader.get_template("front_page/front_page.html")
+
+    return HttpResponse(template.render(context, request))
