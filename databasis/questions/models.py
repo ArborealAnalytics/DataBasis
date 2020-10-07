1faaf3158fa6d9cb9c0074f7e8e3340bb3e03a36
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-10-07 12:19:11 +0300

    Initial Question model

--- databasis/questions/models.py
+++ databasis/questions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    """
+    Represents a question or inquiry that a person may ask.
+
+    Questions may be answered by data or relate to other questions.
+    """
+    title = models.CharField(max_length=255, help_text="The question in short form.")
+    description = models.TextField(help_text="Give a bit more detail about the question.", null=True, blank=True)
