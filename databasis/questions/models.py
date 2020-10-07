d67539c785a9c18e77a95fe24d35e9e1a503bc6c
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-10-07 12:51:31 +0300

    Add Question.__str__()

--- databasis/questions/models.py
+++ databasis/questions/models.py
@@ -9,3 +9,6 @@ class Question(models.Model):
     """
     title = models.CharField(max_length=255, help_text="The question in short form.")
     description = models.TextField(help_text="Give a bit more detail about the question.", null=True, blank=True)
+
+    def __str__(self):
+        return self.title
