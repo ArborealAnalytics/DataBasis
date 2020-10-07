29a08259d0810661d60ffd33be30214b5f06a857
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-10-07 12:56:23 +0300

    Add Question.keywords field

--- databasis/questions/models.py
+++ databasis/questions/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from taggit.managers import TaggableManager
 
 class Question(models.Model):
     """
@@ -9,6 +10,7 @@ class Question(models.Model):
     """
     title = models.CharField(max_length=255, help_text="The question in short form.")
     description = models.TextField(help_text="Give a bit more detail about the question.", null=True, blank=True)
+    keywords = TaggableManager()
 
     def __str__(self):
         return self.title
