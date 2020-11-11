9bad751357a41beb6799d92d0d97b73030cc47d6
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-11 16:26:52 +0200

    Add question CRUD views/links

--- databasis/questions/urls.py
+++ databasis/questions/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
 
-from .views import QuestionsListView
+from .views import (
+    QuestionCreateView,
+    QuestionDeleteView,
+    QuestionDetailView,
+    QuestionsListView,
+    QuestionUpdateView,
+)
 
 urlpatterns = [
+    path("add/", QuestionCreateView.as_view(), name="question_create"),
+    path("<int:pk>/", QuestionDetailView.as_view(), name="question_detail"),
+    path("<int:pk>/update/", QuestionUpdateView.as_view(), name="question_update"),
+    path("<int:pk>/delete/", QuestionDeleteView.as_view(), name="question_delete"),
     path("", QuestionsListView.as_view(), name="questions_index"),
 ]
