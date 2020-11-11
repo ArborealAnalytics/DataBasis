25e86d4c96e05dabf01f69fc669ee9ee1b8f55b8
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-11 18:52:09 +0200

    Rename view to front_page

--- databasis/front_page/urls.py
+++ databasis/front_page/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import index
+from .views import front_page
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", front_page, name="front_page"),
 ]
