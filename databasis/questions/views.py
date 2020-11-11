f5cc1a78b2e539b046b283cd142207beeef4846e
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-11 16:38:26 +0200

    Add view permissions

--- databasis/questions/views.py
+++ databasis/questions/views.py
@@ -1,29 +1,35 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .models import Question
 
-
+@method_decorator(login_required, name='dispatch')
 class QuestionsListView(ListView):
     model = Question
     context_object_name = "questions"
 
+@method_decorator(login_required, name='dispatch')
 class QuestionDetailView(DetailView):
     model = Question
     context_object_name = "question"
 
+@method_decorator(login_required, name='dispatch')
 class QuestionCreateView(CreateView):
     model = Question
     fields = ["title", "description", "keywords"]
 
-
+@method_decorator(login_required, name='dispatch')
 class QuestionUpdateView(UpdateView):
     model = Question
     fields = ["title", "description", "keywords"]
 
-
+@method_decorator(staff_member_required, name='dispatch')
 class QuestionDeleteView(DeleteView):
     model = Question
+    context_object_name = "question"
     success_url = reverse_lazy("questions_index")
