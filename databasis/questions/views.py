54dd9aca37e033804f53ceeb70990adb66f84f94
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-11 16:26:52 +0200

    Add question CRUD views/links

--- databasis/questions/views.py
+++ databasis/questions/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from .models import Question
+
 
 class QuestionsListView(ListView):
     model = Question
     context_object_name = "questions"
+
+class QuestionDetailView(DetailView):
+    model = Question
+    context_object_name = "question"
+
+class QuestionCreateView(CreateView):
+    model = Question
+    fields = ["title", "description", "keywords"]
+
+
+class QuestionUpdateView(UpdateView):
+    model = Question
+    fields = ["title", "description", "keywords"]
+
+
+class QuestionDeleteView(DeleteView):
+    model = Question
+    success_url = reverse_lazy("questions_index")
