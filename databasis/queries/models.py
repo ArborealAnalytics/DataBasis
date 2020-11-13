19fcbd9377a0e8eb2c4d6cccf4c422f96b3413cb
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-13 16:35:39 +0200

    Add Query.history

--- databasis/queries/models.py
+++ databasis/queries/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
+from simple_history.models import HistoricalRecords
+
 
 class Query(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     sql = models.TextField()
+    history = HistoricalRecords()
 
     def __str__(self):
         return self.title
