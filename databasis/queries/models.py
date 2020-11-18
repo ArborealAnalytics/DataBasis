23fdfe6267a55a2038614d82aaf531d14df15e26
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-18 10:54:06 +0200

    Add model docstring

--- databasis/queries/models.py
+++ databasis/queries/models.py
@@ -5,6 +5,11 @@ from taggit.managers import TaggableManager
 
 
 class Query(models.Model):
+    """
+    Represents a database query. 
+    
+    The query may be related to a question, as a means of providing insight.
+    """
     title = models.CharField(max_length=255)
     description = models.TextField()
     sql = models.TextField()
