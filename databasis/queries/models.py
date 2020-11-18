55cf5cb3d2194b5176158cf4ec32c8fb57947442
Brylie Christopher Oxley brylie@amble.fi <brylie@amble.fi>
2020-11-18 10:52:48 +0200

    Add Query.keywords

--- databasis/queries/models.py
+++ databasis/queries/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 from simple_history.models import HistoricalRecords
+from taggit.managers import TaggableManager
 
 
 class Query(models.Model):
@@ -8,6 +9,7 @@ class Query(models.Model):
     description = models.TextField()
     sql = models.TextField()
     history = HistoricalRecords()
+    keywords = TaggableManager()
 
     def __str__(self):
         return self.title
